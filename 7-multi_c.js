c7be10fb357f1a6b0270316af1d3409cdf2acac8
ivyitumbi <itumbiyvonne023@gmail.com>
2025-11-17 00:09:06 +0300

    updated

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
-const times = parseInt(process.argv[2], 10);
 
-if (Number.isNaN(times) || times <= 0) {
-  console.log('Missing number of occurrences');
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
 } else {
-  for (let i = 0; i < times; i++) console.log('C is fun');
+  for (let i = 0; i < x; i++) {
+    console.log("C is fun");
+  }
 }
